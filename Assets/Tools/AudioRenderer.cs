5a3b8b9db7891ff98a97ed8220ed7cbe8cb1d4be
luke2 <r15yokote@gmail.com>
2017-11-26 16:54:26 +0900

    update demo

--- Assets/Tools/AudioRenderer.cs
+++ Assets/Tools/AudioRenderer.cs
@@ -14,20 +14,13 @@ public class AudioRenderer : MonoBehaviour {
   Vector4 ring;
 
   void Start() {
-    wave.OnUpdateVolume += (meter, t) => {
-      volume = meter[0] + meter[1] + meter[2] + meter[3];
-      time = t;
-    };
+    wave.OnUpdateVolume += (meter, time) => UpdateOnAudio (meter, time);
   }
 
-  void Update() {
-    Vector4 amps = new Vector4 (
-      3f * Mathf.Cos(2f * Mathf.PI * time / 15f),
-      3f * Mathf.Cos(2f * Mathf.PI * time / 27f),
-      1f * Mathf.Cos(2f * Mathf.PI * time / 35f),
-      1f * Mathf.Sin(2f * Mathf.PI * time / 55f)
-    );
-    float height = -0.2f - Mathf.PingPong (time, 120f) / 120f - volume + 0.4f;
+  void UpdateOnAudio(float[] meter, float time) {
+    volume = meter[0] + meter[1] + meter[2] + meter[3];
+
+    var height = -0.2f - Mathf.PingPong (time, 120f) / 120f - volume + 0.4f;
 
     var prop = new MaterialPropertyBlock ();
     prop.SetFloat ("_SoundTime", time);
@@ -36,7 +29,6 @@ public class AudioRenderer : MonoBehaviour {
     prop.SetFloat ("_BoxScale", volume * scale);
     prop.SetFloat ("_RingOffset", volume);
 
-    prop.SetVector ("_RingOffset", amps);
     prop.SetFloat ("_HeightOffset", height);
 
     foreach (var rend in GetComponentsInChildren<Renderer>()) {
